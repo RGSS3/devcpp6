ef88ba52a9aab1491a72d66371600fb18840521b
NLTDEV2020 <nltdev@nltgame.cn>
2020-12-08 00:17:33 +0800

    版本更新程序

---
+++ Update/Update.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Update
+{
+    partial class Update
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Update));
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.Color.Orange;
+            this.progressBar.Location = new System.Drawing.Point(8, 8);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(218, 26);
+            this.progressBar.TabIndex = 0;
+            // 
+            // Update
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(234, 41);
+            this.Controls.Add(this.progressBar);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Update";
+            this.Text = "DEV C++ 更新程序";
+            this.Load += new System.EventHandler(this.Update_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
